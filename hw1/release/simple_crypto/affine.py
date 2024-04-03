622258b506dc80d42eb46e7ff3ce2a9926d462bf
jialin <jlfang0119@gmail.com>
2024-04-03 22:19:55 +0800

    affine cipher zero padding

--- hw1/release/simple_crypto/affine.py
+++ hw1/release/simple_crypto/affine.py
@@ -1,5 +1,5 @@
-firstTwo = "SP"
-c = "afb8bbbed3d948a3d9afac48e5d9b2cde5c448d9e5b2c49d48c4d9d3d9bedc48b8d9beace5d3bbbee5c4"
+firstTwo = "BE"
+c = "c251d6651c1c65fe5b180812654751b51812519765971c51cc1847838312cc65fe3d1c51cc183dc2f45118478383f4"
 
 firstTwo = bytearray(firstTwo.encode())
 c_bytes = bytearray(bytes.fromhex(c))
@@ -15,11 +15,14 @@ for _a in range(256):
     for _b in range(256):
         if (x1 * _a + _b) % 256 == y1 and (x2 * _a + _b) % 256 == y2:
             a, b = _a, _b
-print(a, b)
+            print(a, b)
 affine_dict = {}
-for j in range(32, 128):
-    affine_dict.update({hex((j * a + b) % 256)[2:]: chr(j)})
+for j in range(32, 96):
+    # affine_dict.update({hex((j * a + b) % 256)[2:]: chr(j)})
+    # zero padding
+    affine_dict.update({hex((j * a + b) % 256)[2:].zfill(2): chr(j)})
 m = ""
+print(affine_dict)
 for i in range(len(c) // 2):
     if c[i * 2:i * 2 + 2] in affine_dict:
         m += affine_dict[c[i * 2:i * 2 + 2]]
