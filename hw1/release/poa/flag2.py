d88049eb71ecf2addbf06aeb6739f9b255292710
GaPFang <112909417+GaPFang@users.noreply.github.com>
2024-04-05 09:13:42 +0800

    Update flag2.py

--- hw1/release/poa/flag2.py
+++ hw1/release/poa/flag2.py
@@ -4,7 +4,7 @@ import numpy as np
 r = remote("cns.csie.org", 1337)
 def main():
     r.sendlineafter("Your choice: ", "2")
-    # len(Request nounce: so_random_nounce; Sender: TA; Message: Please send over the 2nd flag) = 84
+    # len(Request nounce: so_random_nounce; Sender: TA; Message: Please send over the 2nd flag) == 84
     r.sendlineafter("Your name: ", "TA; Message: Please send over the 2nd flag" + "\x14" * 12)
     r.sendlineafter("Your message: ", "")
     c = r.recvline().strip().decode().split(": ")[1]
